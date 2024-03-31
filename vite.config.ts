2544ad4d21d7a26fad4d10e09d7bb070f8cfbf46
Fernando Bonatto <febonatto_1999@hotmail.com>
2024-03-31 17:13:02 -0300

    chore: Adicionados comandos que alteram o start do dev mode.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '10.0.0.107',
+    port: 3000,
+    open: true,
+  },
   resolve: {
     alias: {
       '@views': path.resolve(__dirname, './src', 'views'),
