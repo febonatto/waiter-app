b9c8c12595c2bfbf15d00ad94e64a4e9fe8b51b2
Fernando Bonatto <febonatto_1999@hotmail.com>
2024-04-02 18:41:23 -0300

    feat: Adicionado react-router-dom e criada primeira versão básica de rotas.

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,6 +1,23 @@
 import '@views/styles/globals.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import { Home } from '@views/pages/Home';
 
 export default function App() {
-  return <Home />;
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />,
+    },
+    {
+      path: '/orders',
+      element: <Home />,
+    },
+    {
+      path: '/me',
+      element: <Home />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
